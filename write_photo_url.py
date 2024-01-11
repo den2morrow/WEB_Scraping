122f361cc8d282e1a6c0fe907ecc46e2e45028fd
naname_me <whattomson@gmail.com>
2024-01-11 13:17:00 +0300

    Добавил обработчик ошибок для функции fetch_url

--- write_photo_url.py
+++ write_photo_url.py
@@ -4,15 +4,18 @@ from bs4 import BeautifulSoup
 
 
 def fetch_url(main_url, url: str = None) -> None:
-    response = requests.get(main_url).text
-    soup = BeautifulSoup(response, 'lxml')
-    photos_on_one_url = [main_url[:-1] + photo_url.find('a').get('href') 
-                        for photo_url in soup.find_all('div', class_='short_prev')]
-    
-    with open('./data/url_for_site_of_picture.txt', 'a') as file:
-        for picture_url in photos_on_one_url:
-            file.write(picture_url + '\n')
-            print(f'Writed: {picture_url}')
+    try:
+        response = requests.get(main_url).text
+        soup = BeautifulSoup(response, 'lxml')
+        photos_on_one_url = [main_url[:-1] + photo_url.find('a').get('href') 
+                            for photo_url in soup.find_all('div', class_='short_prev')]
+        
+        with open('./data/url_for_site_of_picture.txt', 'a') as file:
+            for picture_url in photos_on_one_url:
+                file.write(picture_url + '\n')
+                print(f'Writed: {picture_url}')
+    except Exception as ex:
+        print(f'Error: {ex}')
     
 
 def main():
